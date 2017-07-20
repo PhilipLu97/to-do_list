58221b69baec18a92746fd2fd287ee307f81d385
Philip Lu <philiplu97@gmail.com>
2017-07-20 12:14:17 +0000

    7/20/2017 8:14 AM

--- To-do.js
+++ To-do.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $("#add-new-todo-button").click(function(){
-        var todoBlock = $(this).closest("#new-todo-block");
-        var description = todoBlock.find("#new-todo-description");
-        var pomodoroEstimate = todoBlock.find("#new-pomodoro-estimate");
-        $("ul").append("<li><input type = 'checkbox'/>" + description.val() + "<span class = 'pomodoro-estimate'>"  +  pomodoroEstimate.val()  +  "pomodoros </span></li>");
+    $("#add-new-to-do-button").click(function(){
+        var todoBlock = $(this).closest("#new-to-do-block");
+        var description = todoBlock.find("#new-to-do-description");
+        var PomodoroEstimate = todoBlock.find("#new-Pomodoro-estimate");
+        $("ul").append("<li><input type = 'checkbox'/>" + description.val() + "<span class = 'Pomodoro-estimate'>"  +  PomodoroEstimate.val()  +  "Pomodoros </span></li>");
         description.val("");
-        pomodoroEstimate.val("");
+        PomodoroEstimate.val("");
     });
 });
